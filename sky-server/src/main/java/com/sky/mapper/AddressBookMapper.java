3c0af44f0e0e8f8ee9d35869805d123293ce1b40
zhangchenghao <2282610045@qq.com>
2024-11-05 14:52:31 +0800

    address book api complete

--- sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -3,8 +3,20 @@ package com.sky.mapper;
 import com.sky.entity.AddressBook;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface AddressBookMapper {
 
     void save(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void updateIsDefaultByUserId(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    List<AddressBook> list(AddressBook addressBook);
 }
