ad45d602741bc7876e1f7d863a61f9af77ecb380
zhangchenghao <2282610045@qq.com>
2024-11-06 21:33:27 +0800

    wechat pay api complete

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,4 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface OrderMapper {
     void save(Orders orders);
+
+    Orders getByNumber(String outTradeNo);
+
+    void update(Orders orders);
 }
