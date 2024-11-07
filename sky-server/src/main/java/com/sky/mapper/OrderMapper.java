1b8bf8080bd98e564d4910f642fb138c9e692489
zhangchenghao <2282610045@qq.com>
2024-11-07 14:21:08 +0800

    time task api complete

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,6 +3,9 @@ package com.sky.mapper;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void save(Orders orders);
@@ -10,4 +13,6 @@ public interface OrderMapper {
     Orders getByNumber(String outTradeNo);
 
     void update(Orders orders);
+
+    List<Orders> getByStatusAndOrderTimeLT(Integer pendingPayment, LocalDateTime localDateTime);
 }
---
+++ sky-server/src/main/java/com/sky/task/ObjectTask.java
@@ -0,0 +1,44 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Component
+public class ObjectTask {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Scheduled(cron = "0 * * * * ? ")
+    public void processTimeoutOrder(){
+        log.info("定时处理超时订单{}", LocalDateTime.now());
+        LocalDateTime localDateTime = LocalDateTime.now().minusMinutes(15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT,localDateTime);
+        if(ordersList != null && !ordersList.isEmpty()){
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+    @Scheduled(cron = "0 0 1 * * ? ")
+    public void processDeliveryOrder(){
+        log.info("定时处理处于派送中状态的订单{}", LocalDateTime.now());
+        LocalDateTime localDateTime = LocalDateTime.now().minusMinutes(60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS,localDateTime);
+        if(ordersList != null && !ordersList.isEmpty()){
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
